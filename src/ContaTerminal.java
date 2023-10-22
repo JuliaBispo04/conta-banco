8e5d4d58f96c99d427e7c72c6ef38523637a4a34
Júlia Bispo <143014142+JuliaBispo04@users.noreply.github.com>
2023-10-22 19:42:20 -0300

    ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,23 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Solicitação dos dados da conta
+            System.out.println("Programa: Por favor, digite o número da Agência!");
+            String agencia = scanner.nextLine();
 
-        // Solicitação dos dados da conta
-        System.out.println("Programa: Por favor, digite o número da Agência!");
-        String agencia = scanner.nextLine();
+            System.out.println("Programa: Por favor, digite o número da Conta!");
+            int numeroConta = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Programa: Por favor, digite o número da Conta!");
-        int numeroConta = Integer.parseInt(scanner.nextLine());
+            System.out.println("Programa: Por favor, digite o Nome do Cliente!");
+            String nomeCliente = scanner.nextLine();
 
-        System.out.println("Programa: Por favor, digite o Nome do Cliente!");
-        String nomeCliente = scanner.nextLine();
+            System.out.println("Programa: Por favor, digite o Saldo da Conta!");
+            double saldo = Double.parseDouble(scanner.nextLine());
 
-        System.out.println("Programa: Por favor, digite o Saldo da Conta!");
-        double saldo = Double.parseDouble(scanner.nextLine());
+            // Exibição das informações da conta
+            System.out.println("Olá ".concat(nomeCliente).concat(", obrigado por criar uma conta em nosso banco, sua agência é ").concat(agencia).concat(", conta ").concat(String.valueOf(numeroConta)).concat(" e seu saldo ").concat(String.valueOf(saldo)).concat(" já está disponível para saque."));
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-        // Exibição das informações da conta
-        System.out.println("Olá " + MARIO ANDRADE + ", obrigado por criar uma conta em nosso banco, sua agência é " + 067-8 + ", conta " + 1021 + " e seu saldo " + 237.48 + " já está disponível para saque.");
-
-        scanner.close(); // Feche o scanner quando não precisar mais.
     }
 }
